f70fa171bce57e3f202d6a3e40529b70b125b54e
stulzq <stulzq@qq.com>
2016-07-01 13:05:09 +0800

    sss

---
+++ ZiYi VS2013 Theme Editor/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace NingZiyi.ZiYi_VS2013_Theme_Editor
+{
+    static class GuidList
+    {
+        public const string guidZiYi_VS2013_Theme_EditorPkgString = "e7ad1fa6-30aa-4f46-8ee1-9884fe14ca73";
+        public const string guidZiYi_VS2013_Theme_EditorCmdSetString = "836c41e5-47de-4018-8063-c104eaf1a092";
+
+        public static readonly Guid guidZiYi_VS2013_Theme_EditorCmdSet = new Guid(guidZiYi_VS2013_Theme_EditorCmdSetString);
+    };
+}
